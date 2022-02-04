a6123ba72424cb127be27c48ee57d924f2b1c1d8
raphalopess <raphael.lopes.2013@gmail.com>
2022-02-04 18:37:06 -0300

    Game over condition

--- snake.py
+++ snake.py
@@ -123,3 +123,24 @@ while True:
       food_pos = [random.randrange(1,(frame_size_x // square_size)) * square_size,
             random.randrange(1,(frame_size_y // square_size)) * square_size]   
 
+#GFX
+    game_window.fill(black)
+    for pos in snake_body:
+        pygame.draw.react(game_window, green, pygame.React(
+                pos[0] + 2, pos[1] + 2,
+                    square_size -2, square_size)) 
+    pygame.draw.rect(game_window, red, pygame.React(food_pos[0],
+                        food_pos[1], square_size, square_size))
+
+#game over conditions
+
+    for block in snake_body[1:]:
+        if head_pos[0] == block[0] and head_pos[1] == block[1]:
+            init_vars()
+
+    show_score(1, white, 'consolas', 20)
+    pygame.display.update()
+    fps_controller.tick()
+
+    
+    

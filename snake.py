710c99b6756fa40eecab111abd3fb4a19cb42679
raphalopess <raphael.lopes.2013@gmail.com>
2022-02-04 18:17:45 -0300

    Spawn food

--- snake.py
+++ snake.py
@@ -63,6 +63,63 @@ def init_vars():
     food_pos = [random.randrange(1,(frame_size_x // square_size)) * square_size,
                 random.randrange(1,(frame_size_y // square_size)) * square_size]
     food_spawn = True
+    score = 0
 
+init_vars()
 
+def show_score():
+    print("showing score")
+
+#game loop
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if( event.key == pygame.K_UP or event.key == ord("w")
+                and direction != "DOWN" ):
+                direction = "UP"
+            elif( event.key == pygame.K_UP or event.key == ord("s")
+                and direction != "UP" ):
+                direction = "DOWN"
+            elif( event.key == pygame.K_UP or event.key == ord("a")
+                and direction != "RIGHT" ):
+                direction = "LEFT"
+            elif( event.key == pygame.K_UP or event.key == ord("d")
+                and direction != "LEFT" ):
+                direction = "RIGHT"
+    
+    if direction == "UP":
+        head_pos[1] -= square_size
+    elif direction == "DOWN":
+        head_pos[1] += square_size
+    elif direction == "LEFT":
+        head_pos[0] -= square_size
+    else:
+        head_pos[0] += square_size
+
+    if head_pos[0] < 0:
+        head_pos[0] = frame_size_x - square_size
+    elif head_pos[0] > frame_size_x - square_size:
+        head_pos[0] = 0
+    elif head_pos[1] < 0:
+        head_pos[1] = frame_size_y - square_size
+    elif head_pos[1] > frame_size_y - square_size:
+        head_pos[1] = 0
+
+#eating apple
+
+    snake_body.insert(0,list(head_pos))
+    if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
+
+#spawn food
+    if not food_spawn:
+      food_pos = [random.randrange(1,(frame_size_x // square_size)) * square_size,
+            random.randrange(1,(frame_size_y // square_size)) * square_size]   
 
